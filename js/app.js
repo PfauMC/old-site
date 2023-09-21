4311559b51429086c60b1659aadf8e3a53ddc850
Andrey Pfau <andreypfau@ton.org>
2023-09-21 12:31:14 +0400

    experimental gyroscope x2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener('mousemove', e => {
+	document.getElementsByClassName('xtxt')[0].innerText = "X: " + e.clientX;
+	document.getElementsByClassName('ytxt')[0].innerText = "Y: " + e.clientY;
+
 	Object.assign(document.documentElement, {
 		style: `
 		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
@@ -11,10 +14,15 @@ document.addEventListener('deviceorientation', e => {
 	const x = e.beta;
 	const y = e.gamma;
 
+	document.getElementsByClassName('xtxt')[0].innerText = "X: " + x;
+	document.getElementsByClassName('ytxt')[0].innerText = "Y: " + y;
+	document.getElementsByClassName('ztxt')[0].innerText = "Z: " + z;
+
 	Object.assign(document.documentElement, {
 		style: `
-		--move-x: ${(x - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(y - window.innerHeight / 2) * .01}deg;
+		--move-x: ${((window.innerWidth - ((x + 90) * (window.innerWidth / 180.0))) - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${((window.innerWidth - ((y + 90) * (window.innerWidth / 180.0))) - window.innerWidth / 2) * .01}deg;
 		`
 	})
 })
+
